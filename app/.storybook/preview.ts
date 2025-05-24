ad249ba71d1dcceeefdc383661e1676e6498730d
davimattos <davimattosdev@gmail.com>
2025-05-24 13:01:02 -0300

    feat: storybook configuration

---
+++ app/.storybook/preview.ts
@@ -0,0 +1,15 @@
+import '../src/app/globals.css'; // ajuste esse caminho conforme a estrutura do seu projeto
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
